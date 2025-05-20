adda65da168b63d4354b598be4c85bc320bbb4a0
NemanjaMiloicic <nemanjamiloicice24@gmail.com>
2025-05-20 13:30:04 +0200

    Video background fix

--- Aplikacija/backend/obj/Debug/net8.0/Proba.AssemblyInfo.cs
+++ Aplikacija/backend/obj/Debug/net8.0/Proba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4992232df50e04d4c53391bc80f477ceb6bc7fbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
